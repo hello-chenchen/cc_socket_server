2b0182bd5aa47252e0b2f2e55d440de82f825572
hellochenchen <welcomechenchen@gmail.com>
2017-02-15 02:31:42 +0800

    add SocketManage.java to manage the ServerSocket. But BusinessControl do not known how to handle to receive Msg.

--- CCSocket/src/com/hello_chenchen/Business/BusinessControl.java
+++ CCSocket/src/com/hello_chenchen/Business/BusinessControl.java
@@ -1,10 +1,22 @@
 package com.hello_chenchen.Business;
 
+import com.hello_chenchen.base.SocketServer;
+
 /**
  * Created by hello_chenchen on 2017/2/15.
  * jdk_version:jdk1.8.0_111
  */
-public class BusinessControl {
+public class BusinessControl implements Runnable {
 
+    private SocketManage socketManage;
 
+    public BusinessControl()
+    {
+        socketManage = new SocketManage();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
---
+++ CCSocket/src/com/hello_chenchen/Business/SocketManage.java
@@ -0,0 +1,36 @@
+package com.hello_chenchen.Business;
+
+import com.hello_chenchen.base.SocketServer;
+
+import java.util.HashMap;
+
+/**
+ * Created by root on 17-2-15.
+ * jdk_versio:1.8
+ */
+public class SocketManage {
+
+    private HashMap<Integer , SocketServer> socketMamageMap;
+
+    public SocketManage()
+    {
+        socketMamageMap = new HashMap<Integer, SocketServer>();
+    }
+
+    public void AddSocketServer(int key)
+    {
+        SocketServer value  = new SocketServer(5000);
+        Thread serverThread = new Thread(value);
+        serverThread.start();
+
+        Integer itKey = new Integer(key);
+        socketMamageMap.put(itKey, value);
+    }
+
+    public SocketServer GetSocketServer(int key)
+    {
+        Integer itKey = new Integer(key);
+        SocketServer ret = socketMamageMap.get(itKey);
+        return ret;
+    }
+}
