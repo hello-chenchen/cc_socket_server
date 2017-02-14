7f32d19ed780c470ff42e28cad5689679845027e
hello_chenchen <welcomechenchen@gmail.com>
2017-02-15 00:28:53 +0800

    SocketServer基本框架修改，提供GetReceiveMsg接口获取接收到的消息;新增业务类

---
+++ CCSocket/src/com/hello_chenchen/Business/BusinessControl.java
@@ -0,0 +1,10 @@
+package com.hello_chenchen.Business;
+
+/**
+ * Created by hello_chenchen on 2017/2/15.
+ * jdk_version:jdk1.8.0_111
+ */
+public class BusinessControl {
+
+
+}
--- CCSocket/src/com/hello_chenchen/base/SocketServer.java
+++ CCSocket/src/com/hello_chenchen/base/SocketServer.java
@@ -6,13 +6,19 @@ import java.net.Socket;
 
 /**
  * Created by hello_chenchen on 2017/2/12.
+ * jdk_version:jdk1.8.0_111
  */
 public class SocketServer implements Runnable {
 
     private int port;           //socket连接port
     private ServerSocket serverSocket;  //服务器socket
     private Socket revSocket;
+    private String revMsg = null;
 
+    /**
+     * 构造函数初始化时，实例化ServerScoket
+     * @param Port 监听的端口
+     */
     public SocketServer(int Port){
         this.port   = Port;
 
@@ -22,13 +28,12 @@ public class SocketServer implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void run() {
 
-        while (true) {
+        while (true){
             try {
                 revSocket = serverSocket.accept();
                 System.out.println("Accept...");
@@ -43,15 +48,22 @@ public class SocketServer implements Runnable {
                 e.printStackTrace();
             }
 
-            String revMsg = null;
-
             try {
                 while ((revMsg = revBuffer.readLine()) != null)
                     System.out.println(revMsg);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
 
+        }
+    }
+
+    /**
+     * 获取接收到的消息，有可能是null
+     * @return String接收到的消息
+     */
+    public String getReceiveMsg()
+    {
+        return revMsg;
     }
 }
